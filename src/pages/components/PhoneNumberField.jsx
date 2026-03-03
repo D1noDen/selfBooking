ff706118a5017213a081c5f45c09eabe062c24c5
k0d1kk <leonmiy07@gmail.com>
2026-03-03 11:08:57 +0200

    fix height of symb

--- src/pages/components/PhoneNumberField.jsx
+++ src/pages/components/PhoneNumberField.jsx
@@ -45,16 +45,16 @@ const PhoneNumberField = ({
         {label}
       </label>
 
-      <div className={`${isMobile ? 'border-[1px] border-[#11111333] rounded-[4px]' : "border-[2px] border-[#E8E8E9] rounded-[10px]"} bg-white h-auto py-[5px] px-[8px] flex items-center gap-[10px] focus-within:border focus-within:border-[#6C6AC7]`}>
+      <div className={`${isMobile ? 'border-[1px] border-[#11111333] rounded-[4px]' : "border-[2px] border-[#E8E8E9] rounded-[10px]"} bg-white py-[0px] h-[42px] px-[8px] flex items-center gap-[10px] focus-within:border focus-within:border-[#6C6AC7]`}>
         <div className="relative" ref={dropdownRef}>
           <button
             type="button"
-            className="w-min py-[4px] px-[12px] bg-[#F4F7FF] rounded-[4px] flex items-center justify-between gap-[9px] text-[15px]/[20px] font-[700] text-[#333333]"
+            className="w-min py-[6px] px-[12px] bg-[#F4F7FF] rounded-[4px] flex items-center justify-between gap-[9px] text-[15px]/[20px] font-[700] text-[#333333]"
             onClick={() => setIsOpen((prev) => !prev)}
             aria-label={`${t("select_country_code", "Select country code, current")} ${currentCountry.label}`}
           >
             <span>{selectedCountryCode}</span>
-            <span className={`${isOpen ? "rotate-180" : ""} duration-200`}>
+            <span className={`${isOpen ? "rotate-180" : ""} ml-2 duration-200`}>
               <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 1L6 6L1 1" stroke="#99A1AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
@@ -95,7 +95,7 @@ const PhoneNumberField = ({
           inputMode="numeric"
           pattern="[0-9]*"
           maxLength={currentCountry.maxLength}
-          className="flex-1 h-full outline-none text-[15px]/[20px] text-[#333] font-sans tracking-[0.2px] pr-2 focus:border-none"
+          className="flex-1 h-[90%] bg-transparent outline-none text-[15px]/[20px] text-[#333] font-sans tracking-[0.2px] pr-2 focus:border-none"
           onInput={(event) => {
             event.target.value = event.target.value
               .replace(/\D/g, "")
